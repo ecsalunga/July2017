d1cce54aed02546197587797a24c26ed86fb20f8
Emmanuel Salunga <emmanuel.salunga@gmail.com>
2017-08-18 17:44:21 +0800

    add stock in

--- src/app/product/list/product-list.component.ts
+++ src/app/product/list/product-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Core } from '../../core';
-import { DataLayer } from '../../data';
+import { DataAccess, DataLayer } from '../../data';
 import { ProductInfo } from '../../models';
 
 @Component({
@@ -9,8 +9,16 @@ import { ProductInfo } from '../../models';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+  model: ProductInfo;
+  quantities: Array<number>;
+  selectedQuantity: number = 0;
 
-  constructor(private core: Core, private DL: DataLayer) {}
+  constructor(private core: Core, private DA: DataAccess, private DL: DataLayer) {
+    this.quantities = new Array<number>();
+    for(let x = 1; x <= 100; x++){
+      this.quantities.push(x);
+    }
+  }
 
   SelectProduct(product: ProductInfo) {
     this.DL.Product = product;
@@ -20,6 +28,18 @@ export class ProductListComponent implements OnInit {
   AddProduct(){
     this.DL.Product = null;
     this.core.loadComponent("product-detail");
+  }
+
+  productSelected() {
+    if(this.model)
+      this.selectedQuantity = 1;
+  }
+
+  SuppyProduct() {
+    this.model.Quantity += this.selectedQuantity;
+    this.DA.ProductSave(this.model);
+    this.model = null;
+    this.selectedQuantity = 0;
   }
 
   ngOnInit() { }
